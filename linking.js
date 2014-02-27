c9fe917d9c46ece0b4702afefd03b959e52ad3de
dermatobia <dermatobia@gmail.com>
2014-02-27 01:01:12 -0500

    incorporate graffle code to link up nodes with pretty curvy lines

--- linking.js
+++ linking.js
@@ -1,92 +1,107 @@
+Raphael.fn.connection = function (obj1, obj2, line, bg) {
+    if (obj1.line && obj1.from && obj1.to) {
+        line = obj1;
+        obj1 = line.from;
+        obj2 = line.to;
+    }
+    var bb1 = obj1.getBBox(),
+        bb2 = obj2.getBBox(),
+        p = [{x: bb1.x + bb1.width / 2, y: bb1.y - 1},
+        {x: bb1.x + bb1.width / 2, y: bb1.y + bb1.height + 1},
+        {x: bb1.x - 1, y: bb1.y + bb1.height / 2},
+        {x: bb1.x + bb1.width + 1, y: bb1.y + bb1.height / 2},
+        {x: bb2.x + bb2.width / 2, y: bb2.y - 1},
+        {x: bb2.x + bb2.width / 2, y: bb2.y + bb2.height + 1},
+        {x: bb2.x - 1, y: bb2.y + bb2.height / 2},
+        {x: bb2.x + bb2.width + 1, y: bb2.y + bb2.height / 2}],
+        d = {}, dis = [];
+    for (var i = 0; i < 4; i++) {
+        for (var j = 4; j < 8; j++) {
+            var dx = Math.abs(p[i].x - p[j].x),
+                dy = Math.abs(p[i].y - p[j].y);
+            if ((i == j - 4) || (((i != 3 && j != 6) || p[i].x < p[j].x) && ((i != 2 && j != 7) || p[i].x > p[j].x) && ((i != 0 && j != 5) || p[i].y > p[j].y) && ((i != 1 && j != 4) || p[i].y < p[j].y))) {
+                dis.push(dx + dy);
+                d[dis[dis.length - 1]] = [i, j];
+            }
+        }
+    }
+    if (dis.length == 0) {
+        var res = [0, 4];
+    } else {
+        res = d[Math.min.apply(Math, dis)];
+    }
+    var x1 = p[res[0]].x,
+        y1 = p[res[0]].y,
+        x4 = p[res[1]].x,
+        y4 = p[res[1]].y;
+    dx = Math.max(Math.abs(x1 - x4) / 2, 10);
+    dy = Math.max(Math.abs(y1 - y4) / 2, 10);
+    var x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3),
+        y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3),
+        x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
+        y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
+    var path = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(",");
+    if (line && line.line) {
+        line.bg && line.bg.attr({path: path});
+        line.line.attr({path: path});
+    } else {
+        var color = typeof line == "string" ? line : "#000";
+        return {
+            bg: bg && bg.split && this.path(path).attr({stroke: bg.split("|")[0], fill: "none", "stroke-width": bg.split("|")[1] || 3}),
+            line: this.path(path).attr({stroke: color, fill: "none"}),
+            from: obj1,
+            to: obj2
+        };
+    }
+};
+
 window.onload = function() {
     var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
-    var joe_shape = paper.circle(50, 50, 10).attr({fill: "hsb(0, 1, 1)", stroke: "none"})
-    var sara_shape = paper.circle(100, 100, 10).attr({fill: "hsb(.6, 1, 1)", stroke: "none"})
+    // var joe_shape = paper.circle(50, 50, 10).attr({fill: "hsb(0, 1, 1)", stroke: "none"})
+    // var sara_shape = paper.circle(100, 100, 10).attr({fill: "hsb(.6, 1, 1)", stroke: "none"})
     // var con = paper.path("M 50 50 l 50 50")
-var ox = 0;
-var oy = 0;
+    // var ox = 0;
+    // var oy = 0;
+    connections = [],
+    shapes = [  paper.circle(100, 50, 10),
+                paper.circle(200, 100, 10),
+                paper.circle(250, 150, 10),
+                paper.circle(300, 200, 10) ];
 
-function start(){}
+    for (var i = 0, ii = shapes.length; i < ii; i++) {
+        var color = Raphael.getColor();
+        shapes[i].attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, cursor: "move"});
+        shapes[i].drag(move, start, up);
+    };
 
-function move(dx, dy, posx, posy) {
-    this.attr({
-        transform: "...T" + (dx - ox) + "," + (dy - oy)
-    });
-    paper.connect(joe_shape, sara_shape, "hsb(0, 1, 1)" );
-    // var joe_shape_x = joe_shape.attrs.cx,
-    //     joe_shape_y = joe_shape.attrs.cy,
-    //     sara_shape_x = sara_shape.attrs.cx,
-    //     sara_shape_y = sara_shape.attrs.cy;
-    // con.attr("path", "M"+joe_shape_x+" "+joe_shape_y+" l"+sara_shape_x - joe_shape_x+ " " + sara_shape_y - joe_shape_y);  
-    ox = dx;
-    oy = dy;
-}
+function start(){
+    this.ox = this.type == "rect" ? this.attr("x") : this.attr("cx");
+    this.oy = this.type == "rect" ? this.attr("y") : this.attr("cy");
+    this.animate({"fill-opacity": .2}, 500);
+};
+
+function move(dx, dy) {
+    var att = this.type == "rect" ? {x: this.ox + dx, y: this.oy + dy} : {cx: this.ox + dx, cy: this.oy + dy};
+    this.attr(att);
+    
+    for (var i = connections.length; i--;) {
+            paper.connection(connections[i]);
+        }
+    paper.safari();
+};
 
 
 function up(e) {
-    ox = 0;
-    oy = 0;
-}
+  this.animate({"fill-opacity": 0}, 500);
+};
 
-joe_shape.drag(move, start, up);
-sara_shape.drag(move, start, up);
+  connections.push(paper.connection(shapes[0], shapes[1], "#000"));
+  connections.push(paper.connection(shapes[1], shapes[2], "#000", "#000|1"));
+  connections.push(paper.connection(shapes[2], shapes[3], "#000", "#000|1"));
 
-}
+  // connections.push(paper.connection(joe_shape, sara_shape, "#000"));
+};
 
+// joe_shape.drag(move, start, up);
+// sara_shape.drag(move, start, up);
 
-
-Raphael.fn.connect = function(obj1, obj2, colour) {
-    // list of paths each object has
-     if (!obj1.connections) obj1.connections = []
-     if (!obj2.connections) obj2.connections = []
-     _.each(obj1.connections, function(line){line.remove()})
-   _.each(obj2.connections, function(line){line.remove()})
-
-    // if (!obj1.connections) obj1.connections = [] && if (!obj2.connections) obj2.connections = [])
-    // get the bounding box of each object
-    var box1 = obj1.getBBox()
-    var box2 = obj2.getBBox()
-    // create a line/path from object 1 to object 2
-    var p = this.path("M" + (box1.x + box1.width / 2) + ","
-            + (box1.y + box1.height / 2) + "L " + (box2.x + box2.width / 2)
-            + "," + (box2.y + box2.height / 2))
-    // adjust attributes of the path
-    p.attr({
-        stroke : colour,
-        "stroke-linecap" : "round",
-        "stroke-opacity" : Math.max(obj1.attr('opacity'), obj2.attr('opacity'))
-    })
-    // set the start and end element for this path
-    p.startElement = obj1;
-    p.endElement = obj2;
-    // add the path to each of the object
-    obj1.connections.push(p)
-    obj2.connections.push(p)
-    // mark each object as being connected
-    obj1.connected = true;
-    obj2.connected = true;
-    // listen for the Raphael frame event
-    eve.on("raphael.anim.frame.*", function(obj) {
-        // if the object the frame event is fired on is connected
-        if (this.connected) {
-            // for each connection on this object
-            for ( var c in this.connections) {
-                var path = this.connections[c]; // temp path
-                var b1 = path.startElement.getBBox(); // get the current
-                                                        // location of start
-                                                        // element
-                var b2 = path.endElement.getBBox();// get the current location
-                                                    // of end element
-                // move the path to the new locations
-                path.attr({
-                    path : "M " + (b1.x + b1.width / 2) + " "
-                            + (b1.y + b1.height / 2) + "L "
-                            + (b2.x + b2.width / 2) + " "
-                            + (b2.y + b2.height / 2),
-                    opacity : Math.max(path.startElement.attr('opacity'),
-                            path.endElement.attr('opacity'))
-                });
-            }
-        }
-    });
-}
